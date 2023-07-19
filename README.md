b17cc3859e21165698f1f8dbd66a4b0a7d7255d9
stmSi <stm1998sithumyo@gmail.com>
2023-07-19 12:22:06 +0630

    Release 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-### WIP
+## Godot Engine Fetcher
+
+#### Installation
+Either download the binary file github release or
+<br >
+Build locally using `cargo`.
+- `cargo build`
+- `cargo run`
+
+#### 1. Selecting Godot Version
+![01](img/01-selecting-version.png)
+
+
+#### 2. Selecting Either Subversion or Download Stable build
+![02](img/02-selecting-subversion-or-builds.png)
+
+#### 3. Downloading in progress
+![03](img/03-download-build.png)
+
+#### 4. After downloading currect directory should have godot engine file. 
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 use std::format;
-use std::{io::stdin, println, process::exit};
+use std::fs::File;
+use std::{println, process::exit};
 
-use reqwest::blocking::Client;
-use reqwest::blocking::Response;
-use reqwest::Client as NonBlockingClient;
-use reqwest::Response as NonBlockingResponse;
+use std::cmp::min;
+use std::io::Write;
+
+use reqwest::Client;
+use reqwest::Response;
 use scraper::{Html, Selector};
 
 use log::error;
 use log::info;
 use log::LevelFilter;
 
-fn main() {
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+
+use dialoguer::{theme::ColorfulTheme, Select};
+
+#[tokio::main]
+async fn main() {
     simple_logger::SimpleLogger::new()
         .with_level(LevelFilter::Info)
         .init()
         .unwrap();
     let client = Client::new();
 
-    let tuxfamily_link = "https://downloads.tuxfamily.org/godotengine";
+    let mut tuxfamily_link = "https://downloads.tuxfamily.org/godotengine".to_string();
 
     info!("Fetching '{}' ", &tuxfamily_link);
 
-    let res = client.get(tuxfamily_link).send().unwrap_or_else(|err| {
-        error!("fetching godot's tuxfamily link: {}", err);
-        exit(1);
-    });
+    let res = client
+        .get(&tuxfamily_link)
+        .send()
+        .await
+        .unwrap_or_else(|err| {
+            error!("fetching godot's tuxfamily link: {}", err);
+            exit(1);
+        });
 
     let filters = vec![
         "Parent Directory".to_string(),
@@ -34,67 +46,50 @@ fn main() {
         "media".to_string(),
         "testing".to_string(),
     ];
-    let mut versions = get_choices_from_html_tags(res, filters);
+    let mut versions = get_choices_from_html_tags(res, filters).await;
 
     // Show Latest Version First.
     versions.reverse();
 
-    let selected_version = choose(&versions);
+    // loop and keep fetching subversions until we reach the endpoint file tar.xz or zip.
+    loop {
+        let selected_version = choose(&mut versions);
 
-    println!("You selected version: {}", selected_version);
+        println!("You selected version: {}", selected_version);
 
-    let subversion_url = format!("{}/{}/", tuxfamily_link, selected_version);
+        tuxfamily_link = format!("{}/{}", tuxfamily_link, selected_version).to_string();
 
-    // Fetch subversions
-    let resp = client.get(subversion_url).send().unwrap_or_else(|err| {
-        error!("fetching godot's subversion tuxfamily link: {}", err);
-        exit(1);
-    });
+        if tuxfamily_link.contains(".tar.xz")
+            || tuxfamily_link.contains(".zip")
+            || tuxfamily_link.contains(".aar")
+        {
+            break;
+        }
 
-    let filters = vec!["Parent Directory".to_string()];
-    let mut subversions = get_choices_from_html_tags(resp, filters);
+        // Fetch subversions
+        info!("Fetching '{}' ", &tuxfamily_link);
+        let resp = client
+            .get(&tuxfamily_link)
+            .send()
+            .await
+            .unwrap_or_else(|err| {
+                error!("fetching godot's subversion tuxfamily link: {}", err);
+                exit(1);
+            });
 
-    // Show Latest Sub Version First.
-    subversions.reverse();
-    let selected_subversion = choose(&subversions);
+        let filters = vec!["Parent Directory".to_string()];
+        versions = get_choices_from_html_tags(resp, filters).await;
 
-    let platform_url = format!(
-        "{}/{}/{}/",
-        tuxfamily_link, selected_version, selected_subversion
-    );
-    let resp = client.get(platform_url).send().unwrap_or_else(|err| {
-        error!("fetching godot's tuxfamily link: {}", err);
-        exit(1);
-    });
+        // Show Latest Sub Version First.
+        versions.reverse();
+    }
 
-    let filters = vec!["Parent Directory".to_string()];
-    let platforms = get_choices_from_html_tags(resp, filters);
-    let selected_platform = choose(&platforms);
-    let _ = download_and_extract(format!(
-        "{}/{}/{}/{}/",
-        tuxfamily_link, selected_version, selected_subversion, selected_platform
-    ));
+    info!("Fetching File: '{}' ", &tuxfamily_link);
+    let _ = download_file(&tuxfamily_link).await;
 }
 
-async fn download_and_extract(download_url: String) {
-    let client = NonBlockingClient::new();
-    info!("Downloading '{}'", download_url);
-
-    let resp = client
-        .get(&download_url)
-        .send()
-        .await
-        .unwrap_or_else(|err| {
-            error!("Failed to GET from '{}'", &download_url);
-            exit(-1);
-        });
-    let total_size = resp
-        .content_length()
-        .ok_or(format!("Failed to get content length from '{}'", &download_url));
-}
-
-fn get_choices_from_html_tags(resp: Response, filters: Vec<String>) -> Vec<String> {
-    let body = match resp.text() {
+async fn get_choices_from_html_tags(resp: Response, filters: Vec<String>) -> Vec<String> {
+    let body = match resp.text().await {
         Ok(text_body) => text_body,
         Err(err) => {
             error!("{}", err);
@@ -122,60 +117,61 @@ fn get_choices_from_html_tags(resp: Response, filters: Vec<String>) -> Vec<Strin
         }
     }
 
-    return versions;
+    versions
 }
 
-fn choose(choices: &Vec<String>) -> String {
-    let mut page = 0;
-    let per_page = 9;
-    loop {
-        for i in (page * per_page)..((page + 1) * per_page) {
-            if i < choices.len() {
-                println!("{}: {}", i + 1, choices[i]);
-            }
-        }
+pub async fn download_file(download_url: &str) -> Result<(), String> {
+    // get filename with extension from url
+    let path = download_url.split('/').last().unwrap_or_else(|| {
+        error!("Failed to get filename from '{}'", &download_url);
+        exit(-1);
+    });
 
-        let more = ((page + 1) * per_page) < choices.len();
-        let mut start = 1;
-        if more {
-            println!("0: More..");
-            start = 0;
-        }
+    let client = Client::new();
+    // Reqwest setup
+    let res = client
+        .get(download_url.clone())
+        .send()
+        .await
+        .or(Err(format!("Failed to GET from '{}'", &download_url)))?;
+    let total_size = res.content_length().ok_or(format!(
+        "Failed to get content length from '{}'",
+        &download_url
+    ))?;
 
-        let mut choice: u8;
+    // Indicatif setup
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .unwrap()
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", download_url));
 
-        // Loop forever until user input correct choice.
-        loop {
-            println!(
-                "Please select a with number[{} - {}]: ",
-                start,
-                choices.len()
-            );
+    // download chunks
+    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
 
-            let mut input = String::new();
-            stdin().read_line(&mut input).unwrap();
-
-            choice = match input.trim().parse() {
-                Ok(choice) => choice,
-                Err(err) => {
-                    error!("Parsing user input: {}", err);
-                    continue;
-                }
-            };
-
-            if usize::from(choice) > choices.len() {
-                error!("Invalid Choice. Try again.");
-                continue;
-            }
-
-            break;
-        }
-
-        if choice == 0 {
-            page += 1;
-            continue;
-        }
-
-        return choices[usize::from(choice) - 1].clone();
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write_all(&chunk)
+            .or(Err(format!("Error while writing to file")))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
     }
+
+    pb.finish_with_message(format!("Downloaded {} to {}", download_url, path));
+    Ok(())
+}
+
+fn choose(choices: &mut [String]) -> String {
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select a version")
+        .default(0)
+        .items(choices)
+        .interact()
+        .unwrap();
+
+    choices[selection].clone()
 }
